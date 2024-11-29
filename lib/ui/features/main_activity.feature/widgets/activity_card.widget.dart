024a7041ead5c2c91bc45a257999540e38aa56e4
jrdeavila <jrdeavila@unicesar.edu.co>
2024-11-29 09:45:21 -0500

    Refactor ActivityCard widget for improved layout and readability; replace Container with AnimatedContainer, streamline child widgets, and adjust padding

--- lib/ui/features/main_activity.feature/widgets/activity_card.widget.dart
+++ lib/ui/features/main_activity.feature/widgets/activity_card.widget.dart
@@ -16,7 +16,7 @@ class ActivityScrollableList extends StatelessWidget {
     return Row(
       children: [
         const ActivityScrollableDecorator(),
-        const SizedBox(width: 16),
+        const SizedBox(width: 5),
         Expanded(
           child: Column(
             children: [
@@ -24,6 +24,7 @@ class ActivityScrollableList extends StatelessWidget {
               const SizedBox(height: 16),
               Expanded(
                 child: SingleChildScrollView(
+                  padding: const EdgeInsets.all(16),
                   child: Column(
                     children: activityList.map((activity) {
                       return ActivityCard(activity: activity);
@@ -104,16 +105,36 @@ class ActivityCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    final isDark = Get.find<ThemeController>().isDarkMode;
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
       padding: const EdgeInsets.all(16),
       margin: const EdgeInsets.only(bottom: 16),
       decoration: BoxDecoration(
-        border: Border.all(
-          color: Get.isDarkMode
-              ? Get.find<AppColors>().neutral600
-              : Get.find<AppColors>().white,
-          width: 1,
-        ),
+        border: isDark
+            ? Border.all(
+                color: Get.find<AppColors>().neutral600,
+                width: 1,
+              )
+            : null,
+        boxShadow: isDark
+            ? null
+            : [
+                BoxShadow(
+                  color: Get.find<AppColors>().black.withOpacity(0.05),
+                  offset: const Offset(-1, -1),
+                  blurRadius: 5,
+                  spreadRadius: 0,
+                ),
+                BoxShadow(
+                  color: Get.find<AppColors>().black.withOpacity(0.15),
+                  offset: const Offset(3, 3),
+                  blurRadius: 12,
+                  spreadRadius: 5,
+                ),
+              ],
+        color:
+            isDark ? Get.find<AppColors>().blackB : Get.find<AppColors>().white,
         borderRadius: BorderRadius.circular(10),
       ),
       child: Row(
@@ -122,113 +143,102 @@ class ActivityCard extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                _buildTimeDuration(context),
+                Row(
+                  children: [
+                    Text(
+                      activity.formattedStartTime,
+                      style: Theme.of(context).textTheme.displaySmall,
+                    ),
+                    const SizedBox(width: 8),
+                    Text(
+                      "(${activity.formattedDuration})",
+                      style: Theme.of(context).textTheme.labelSmall,
+                    ),
+                  ],
+                ),
                 const SizedBox(height: 8),
                 Text(
                   activity.title,
                   style: Theme.of(context).textTheme.titleLarge,
                 ),
                 const SizedBox(height: 4),
-                _buildPlaceTarget(context),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(FontAwesomeIcons.mapMarkerAlt,
+                        size: 12, color: Get.find<AppColors>().neutral500),
+                    const SizedBox(width: 4),
+                    Text(
+                      activity.place,
+                      style: Theme.of(context).textTheme.labelSmall,
+                    ),
+                  ],
+                ),
                 const SizedBox(height: 4),
-                _buildSpotsAndTags(context)
+                Row(
+                  children: [
+                    Icon(FontAwesomeIcons.user,
+                        size: 10, color: Get.find<AppColors>().neutral500),
+                    const SizedBox(width: 4),
+                    Text(
+                      activity.formattedParticipants,
+                      style: Theme.of(context).textTheme.labelSmall,
+                    ),
+                    const SizedBox(width: 8),
+                    ...activity.details.map((detail) {
+                      return Container(
+                        margin: const EdgeInsets.only(right: 4),
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 3,
+                          horizontal: 7,
+                        ),
+                        decoration: BoxDecoration(
+                          color:
+                              getColorFromActivityDetail(detail)["background"],
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                        child: Text(
+                          getStringFromActivityDetail(detail),
+                          style: Theme.of(context)
+                              .textTheme
+                              .labelSmall
+                              ?.copyWith(
+                                color:
+                                    getColorFromActivityDetail(detail)["title"],
+                                fontWeight: FontWeight.bold,
+                              ),
+                        ),
+                      );
+                    }),
+                  ],
+                )
               ],
             ),
           ),
-          _buildPriceJoinButton(context),
+          Column(
+            children: [
+              Text(
+                activity.formattedPrice,
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              const SizedBox(height: 8),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 20,
+                    horizontal: 20,
+                  ),
+                ),
+                child: Text(
+                  Get.find<AppStrings>().activityCardActionJoin.tr,
+                ),
+              ),
+            ],
+          ),
         ],
       ),
-    );
-  }
-
-  Column _buildPriceJoinButton(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          activity.formattedPrice,
-          style: Theme.of(context).textTheme.titleLarge,
-        ),
-        const SizedBox(height: 8),
-        ElevatedButton(
-          onPressed: () {},
-          style: ElevatedButton.styleFrom(
-            padding: const EdgeInsets.symmetric(
-              vertical: 20,
-              horizontal: 20,
-            ),
-          ),
-          child: Text(
-            Get.find<AppStrings>().activityCardActionJoin.tr,
-          ),
-        ),
-      ],
-    );
-  }
-
-  Row _buildSpotsAndTags(BuildContext context) {
-    return Row(
-      children: [
-        Icon(FontAwesomeIcons.user,
-            size: 10, color: Get.find<AppColors>().neutral500),
-        const SizedBox(width: 4),
-        Text(
-          activity.formattedParticipants,
-          style: Theme.of(context).textTheme.labelSmall,
-        ),
-        const SizedBox(width: 8),
-        ...activity.details.map((detail) {
-          return Container(
-            margin: const EdgeInsets.only(right: 4),
-            padding: const EdgeInsets.symmetric(
-              vertical: 3,
-              horizontal: 7,
-            ),
-            decoration: BoxDecoration(
-              color: getColorFromActivityDetail(detail)["background"],
-              borderRadius: BorderRadius.circular(4),
-            ),
-            child: Text(
-              getStringFromActivityDetail(detail),
-              style: Theme.of(context).textTheme.labelSmall?.copyWith(
-                    color: getColorFromActivityDetail(detail)["title"],
-                    fontWeight: FontWeight.bold,
-                  ),
-            ),
-          );
-        }),
-      ],
-    );
-  }
-
-  Row _buildPlaceTarget(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Icon(FontAwesomeIcons.mapMarkerAlt,
-            size: 12, color: Get.find<AppColors>().neutral500),
-        const SizedBox(width: 4),
-        Text(
-          activity.place,
-          style: Theme.of(context).textTheme.labelSmall,
-        ),
-      ],
-    );
-  }
-
-  Row _buildTimeDuration(BuildContext context) {
-    return Row(
-      children: [
-        Text(
-          activity.formattedStartTime,
-          style: Theme.of(context).textTheme.displaySmall,
-        ),
-        const SizedBox(width: 8),
-        Text(
-          "(${activity.formattedDuration})",
-          style: Theme.of(context).textTheme.labelSmall,
-        ),
-      ],
     );
   }
 }
